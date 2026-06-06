a6845810fc5f8eacc46a8b8b012a3e8955ab8ab7
Антон Скляров <sklarovanton833@gmail.com>
2026-06-06 15:43:50 +0500

    Task3

---
+++ Tyuiu.SklyarovAM.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SklyarovAM.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b) 
+        {  
+            return a + b; 
+        }
+    }
+}
---
+++ Tyuiu.SklyarovAM.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SklyarovAM.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SklyarovAM.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44b18fab-655a-4746-a7ff-a9b1736784a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
